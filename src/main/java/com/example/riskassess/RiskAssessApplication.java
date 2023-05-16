229835d776b3e695119eb9b6c88d75c3b2236167
zxy <727387373@qq.com>
2023-05-16 23:48:33 +0800

    first

---
+++ src/main/java/com/example/riskassess/RiskAssessApplication.java
@@ -0,0 +1,13 @@
+package com.example.riskassess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskAssessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskAssessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/riskassess/RiskAssessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riskassess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskAssessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
